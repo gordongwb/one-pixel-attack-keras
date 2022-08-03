c139f7304b14c98552c30a60b605a769d12bb0ca
wicsp <50473954+wicsp@users.noreply.github.com>
2022-08-03 20:19:29 +0800

    Update network_in_network.py

--- networks/network_in_network.py
+++ networks/network_in_network.py
@@ -5,8 +5,9 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, AveragePooling2D
-from keras.initializers import RandomNormal  
-from keras.layers.normalization import BatchNormalization
+from keras.initializers import RandomNormal
+# from keras.layers.normalization import BatchNormalization
+from keras.layers.normalization.batch_normalization_v1 import BatchNormalization
 from keras import optimizers
 from keras.callbacks import LearningRateScheduler, TensorBoard, ModelCheckpoint
 
